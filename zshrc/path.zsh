b56547ba23c865970e3327bfccc0ce96e90ebd1c
Bryan Hu <bryan.hu.2020@gmail.com>
2021-11-13 10:33:04 -0800

    :sparkles: Added yarn to $PATH

--- zshrc/path.zsh
+++ zshrc/path.zsh
@@ -1,4 +1,4 @@
-NEW_PATH="$PYENV_ROOT/bin:$HOME/.local/bin:$HOME/bin:$HOME/.cargo/bin:$HOME/.gem/ruby/2.6.0/bin"  # Add binary stuff
+NEW_PATH="$PYENV_ROOT/bin:$HOME/.local/bin:$HOME/bin:$HOME/.cargo/bin:$HOME/.gem/ruby/2.6.0/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"  # Add binary stuff
 NEW_PATH="$NEW_PATH:$(echo $HOME/.bootstrap/* | sed 's/ /:/g')" # Add bootstraps
 NEW_PATH="$NEW_PATH:/Applications/Postgres.app/Contents/Versions/latest/bin"  # Add PostgreSQL stuff
 NEW_PATH="$NEW_PATH:$HOME/homebrew/bin"  # Add HomeBrew last
