77ed44d85f11bc47c2dfa6a27e4f8d39993eac37
Bryan Hu <bryan.hu.2020@gmail.com>
2021-11-25 11:21:38 -0800

    :zap: Reified pyenv path init

--- zshrc/path.zsh
+++ zshrc/path.zsh
@@ -1,7 +1,6 @@
-NEW_PATH="$PYENV_ROOT/bin:$HOME/.local/bin:$HOME/bin:$HOME/.cargo/bin:$HOME/.gem/ruby/2.6.0/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"  # Add binary stuff
+NEW_PATH="$PYENV_ROOT/bin:/Users/bryanhu/.pyenv/shims:$HOME/.local/bin:$HOME/bin:$HOME/.cargo/bin:$HOME/.gem/ruby/2.6.0/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"  # Add binary stuff
+
 NEW_PATH="$NEW_PATH:$(echo $HOME/.bootstrap/* | sed 's/ /:/g')" # Add bootstraps
 NEW_PATH="$NEW_PATH:/Applications/Postgres.app/Contents/Versions/latest/bin"  # Add PostgreSQL stuff
 NEW_PATH="$NEW_PATH:$HOME/homebrew/bin"  # Add HomeBrew last
 export PATH="$NEW_PATH:$PATH"
-
-eval "$(pyenv init --path)"
