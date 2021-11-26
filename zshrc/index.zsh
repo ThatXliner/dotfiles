6d76616cd4edfedc55e1d8a315f1f31efe85f828
Bryan Hu <bryan.hu.2020@gmail.com>
2021-11-25 19:55:23 -0800

    :sparkles: I want to be able to use my stuff

--- zshrc/index.zsh
+++ zshrc/index.zsh
@@ -16,4 +16,6 @@ rm -rf $HOME/.pylint.d
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 unalias _
 
+export PATH="$HOME/.nvm/versions/node/$(cat ~/.nvm/alias/$(cat ~/.nvm/alias/lts/\*))/bin:$PATH"
+
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh  # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
