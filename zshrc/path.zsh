b111fe1115c0aace5417642e9d01b03542f3e90f
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-01 17:10:26 -0800

    Rust!

--- zshrc/path.zsh
+++ zshrc/path.zsh
@@ -5,6 +5,7 @@
 NEWPATH="$HOME/.bin:$HOME/.local/bin"
 NEWPATH+=":/Applications/Postgres.app/Contents/Versions/latest/bin"  # psql
 NEWPATH+=":$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
+NEWPATH+=":$HOME/.cargo/bin"  # Rust
 # Bun and globally installed tools using Bun
 NEWPATH+=":$HOME/.bun/bin"
 # pnpm installation
