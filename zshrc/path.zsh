c12080ea00dd959c7745b89362a07ae2711045b8
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-20 21:18:57 -0800

    Path bin

--- zshrc/path.zsh
+++ zshrc/path.zsh
@@ -2,7 +2,7 @@
 # things such as SDKMAN! and possibly pnpm
 
 ## $PATH-only modifications ##
-NEWPATH="$HOME/.bin:$HOME/.local/bin"
+NEWPATH="$HOME/bin:$HOME/.local/bin"
 NEWPATH+=":/Applications/Postgres.app/Contents/Versions/latest/bin"  # psql
 NEWPATH+=":$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
 NEWPATH+=":$HOME/.cargo/bin"  # Rust
