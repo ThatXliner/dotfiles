f2100017e81c1f2b2ce422ba358672cf51fe26a9
ThatXliner <bryan.hu.2020@gmail.com>
2024-11-18 19:36:54 -0800

    :fire: no direnv

--- zshrc/path.zsh
+++ zshrc/path.zsh
@@ -28,14 +28,6 @@ NEWPATH+=":$HOME/Applications/VisualVM.app/Contents/MacOS"
 NEWPATH+=":$HOME/.docker/bin"
 export PATH="$NEWPATH:$PATH"
 
-## Scripts ##
-
-# asdf-direnv
-export DIRENV_LOG_FORMAT=""
-[[ -f "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc" ]] && source "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc"
-# I don't know the relationship between asdf and direnv
-# but I'm guessing it's pretty complicated (just like me and K!)
-
 # SDKMAN!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
