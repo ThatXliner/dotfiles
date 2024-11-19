ca2a9e4e68b94431604b1323f4192b7ab43e478a
ThatXliner <bryan.hu.2020@gmail.com>
2024-11-18 19:38:01 -0800

    updated pythonrc (Python 3.13 is GOATED)

--- .pythonrc.py
+++ .pythonrc.py
@@ -5,5 +5,5 @@ import sys
 # sys.path.insert(0, "/Users/bryanhu/.local/pipx/venvs")
 # importlib.import_module("rich.pretty").install()
 # A red prompt and a cyan continuing prompt
-sys.ps1 = "\r\033[31m>>> \033[0m"
-sys.ps2 = "\r\033[36m... \033[0m"  # TODO: Make purple
+#sys.ps1 = "\r\033[31m>>>\033[0m"
+#sys.ps2 = "\r\033[36m...\033[0m"  # TODO: Make purple
