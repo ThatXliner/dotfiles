75d89efa8763665523edec8f5d9285e11f533134
ThatXliner <bryan.hu.2020@gmail.com>
2025-01-05 08:51:47 -0800

    nvim

---
+++ nvim/lua/community.lua
@@ -0,0 +1,21 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.motion.nvim-tree-pairs" },
+  -- { import = "astrocommunity.pack.nvchad-ui" },
+  -- import/override with your plugins folder
+}
---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  },
+}
