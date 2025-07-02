85893254fce7343a526671bcffd36db3a7933e61
ThatXliner <bryan.hu.2020@gmail.com>
2025-07-01 22:32:57 -0500

    img

--- zshrc/path.zsh
+++ zshrc/path.zsh
@@ -14,6 +14,9 @@ NEWPATH+=":$HOME/Applications/VisualVM.app/Contents/MacOS"
 NEWPATH+=":$HOME/.docker/bin"
 # Julia
 NEWPATH+=":$HOME/.juliaup/bin"
+# Darktable & RawTherapee
+NEWPATH+=":$HOME/Applications/darktable.app/Contents/MacOS"
+NEWPATH+=":/Applications/RawTherapee.app/Contents/MacOS"
 # Wasmer
 export WASMER_DIR="$HOME/.wasmer"
 [ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
