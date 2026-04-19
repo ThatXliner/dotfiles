918b5bcbc1978a5d62581489717872768600f20a
ThatXliner <bryan.hu.2020@gmail.com>
2026-04-19 07:49:16 -0700

    Add tools and update Claude aliases

--- zshrc/aliases.zsh
+++ zshrc/aliases.zsh
@@ -21,8 +21,9 @@ alias clean_zsh='zsh -f'
 
 alias lsusb_messy='ioreg -p IOUSB -l -w 0'
 alias lsusb_clean='system_profiler SPUSBDataType'
-alias claude='SHELL=/bin/bash \claude'
+alias claude='SHELL=/bin/bash \claude --model claude-opus-4-5'  # Use Opus 4.5 since it's anecdotally more consistent
 alias strongclaude='CLUADE_CODE_EFFORT_LEVEL=max CLAUDE_CODE_DISABLE_ADAPTIVE_THINKING=1 CLAUDE_CODE_EXPERIMENTAL_AGENT_TEAMS=1 claude --dangerously-skip-permissions'
+alias claudewithdiscord='claude --channels plugin:discord@claude-plugins-official --dangerously-skip-permissions'
 alias commit="claude --dangerously-skip-permissions --print '/x-commit'"
 
 alias gm='git commit -m '
