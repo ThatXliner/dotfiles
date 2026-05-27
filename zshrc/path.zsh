5155cfe733e41d35a121f9d8e73b3526f251b79f
Bryan Hu <me@x-frigate.lan>
2026-05-26 18:33:56 -0700

    Remove JetBrains, Docker, Julia, RawTherapee, and Wasmer from PATH
    Also add cache seeding step to setup and update README with API key docs

--- zshrc/path.zsh
+++ zshrc/path.zsh
@@ -5,18 +5,8 @@
 NEWPATH="$HOME/bin:$HOME/.local/bin"
 NEWPATH+=":$HOME/Applications/Postgres.app/Contents/Versions/latest/bin"  # psql
 NEWPATH+=":$HOME/.cargo/bin"  # Rust
-# JetBrains Toolbox
-NEWPATH+=":$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
-# Docker binaries
-NEWPATH+=":$HOME/.docker/bin"
-# Julia
-NEWPATH+=":$HOME/.juliaup/bin"
-# Darktable & RawTherapee
+# Darktable
 NEWPATH+=":$HOME/Applications/darktable.app/Contents/MacOS"
-NEWPATH+=":/Applications/RawTherapee.app/Contents/MacOS"
 # Bun (these scripts have auto-updaters bro)
 NEWPATH+=":$HOME/.bun/bin"
-# Wasmer
-export WASMER_DIR="$HOME/.wasmer"
-[ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
 export PATH="$NEWPATH:$PATH"
